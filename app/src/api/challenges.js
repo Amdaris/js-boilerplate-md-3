91d15f8d1d0b0e553beff38caca00db68d7eebe2
Oleg Rotar <oleg.rotar@amdaris.com>
2025-03-06 20:30:36 +0200

    Initial commit

---
+++ app/src/api/challenges.js
@@ -0,0 +1,20 @@
+function challenge1(array) {
+    // Write the code for Challenge 1 here
+
+    return -1;
+}
+
+
+function challenge2(array) {
+    // Write the code for Challenge 2 here
+
+    return [];
+}
+
+function challenge3(array) {
+    // Write the code for Challenge 3 here
+
+    return true;
+}
+
+module.exports = { challenge1, challenge2, challenge3 };
